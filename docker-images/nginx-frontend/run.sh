2a0362505c42bb4fa2362be5f893bc1b0cf630a1
Jan Kaluza <jkaluza@redhat.com>
2015-07-01 15:44:54 +0200

    Add nginx-frontend

---
+++ docker-images/nginx-frontend/run.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Author: Jan Kaluza <jkaluza at redhat dot com>
+# Description: Wait until all webapps become ready, generate the httpd config
+# file and start the httpd.
+
+/kubernetes-confd "/etc/nginx/conf.d" "/nginx-cfg /etc/nginx/conf.d /etc/nginx/conf.d" "killall nginx" &
+
+while : ; do
+    nginx -g "daemon off;"
+    sleep 1
+done
